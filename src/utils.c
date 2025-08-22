85495b712d1619d839164cf38dc56fca3245bf3d
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-22 16:53:27 +0900

    Added: convert_argv_to_integers()

---
+++ src/utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 16:09:25 by squinn            #+#    #+#             */
+/*   Updated: 2025/08/22 16:52:55 by squinn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+/*
+* 入力：文字列の配列、出力する配列のサイズ
+* 出力：各文字列を数字に変換して、その数字を入れた整数の配列
+*/
+int *convert_argv_to_integers(char **words, int size) {
+  int *result = malloc(sizeof(int) * size);
+  int i = 0;
+  while (i < size) {
+    int num = ft_atoi(words[i]);
+    result[i] = num;
+    i++;
+  }
+  return result;
+}
