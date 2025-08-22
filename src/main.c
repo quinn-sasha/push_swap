234168b78199884bda7cba1fa0fde170fe254e70
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-08-22 20:19:31 +0900

    Modifie: initialize_stack() argument

--- src/main.c
+++ src/main.c
@@ -6,35 +6,43 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 13:48:59 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/22 17:01:22 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/22 19:10:26 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
 /*
-*
-*
+* nodeのメンバである data, sorted_index, next, prev を初期化する
+* 副作用：int *numsを動的に割り当てて、解放する
 */
-void initialize_nodes(t_node *nodes) {
-
+void initialize_nodes(t_node *nodes, int size, char *input_nums) {
+  int *nums = convert_argv_to_integers(input_nums, size);
+  int i = 0;
+  while (i < size) {
+    nodes[i].data = nums[i];
+    nodes[i].next = NULL;
+    nodes[i].prev = NULL;
+    i++;
+  }
 }
 
 /*
-* 入力：初期化されていないスタック, 整数の配列、整数のサイズ
+* 入力：初期化されていないスタック, スタックの要素数、プログラムへの引数
 * 出力：なし
-* 副作用：stack構造体のメンバである head, size, max_sizeを初期化する
+* 副作用：
+  * stack構造体のメンバである head, size, max_sizeを初期化する
+  * t_node *nodesを動的に割り当てて、解放する
 */
-void initialize_stack(t_stack *stack, int *nums, int max_size) {
+void initialize_stack(t_stack *stack, int max_size, char *input_nums) {
   stack->max_size = max_size;
   stack->size = 0;
   stack->head = NULL;
   t_node *nodes = malloc(sizeof(t_node) * max_size);
-  initialize_nodes(nodes);
+  initialize_nodes(nodes, max_size, input_nums);
   int i = 0;
   while (i < max_size) {
-    // node = nodes[i]
-    // push(stack, node)
+    // push(stack, nodes[i])
     i++;
   }
 }
@@ -43,6 +51,5 @@ int main(int argc, char *argv[]) {
   // TODO: validate input
   t_stack stack1;
   t_stack stack2;
-  int *nums = convert_argv_to_integers(argv + 1, argc - 1);
-  initialize_stack(&stack1, nums, argc - 1);
+  initialize_stack(&stack1, argc - 1, argv + 1);
 }
