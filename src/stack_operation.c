71191798eb4cc6a5cbdbb1311396ef88ed678d52
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-23 11:37:23 +0900

    Fixed: stack->head was not changed

--- src/stack_operation.c
+++ src/stack_operation.c
@@ -6,22 +6,29 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 09:10:06 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/23 10:15:02 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/23 11:08:07 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+/*
+* 入力：stack構造体へのポインタ、node構造体へのポインタ
+* 副作用：
+*   循環リストの先頭をnodeにする。
+*   スタックのサイズを1増やす。
+*/
 void push(t_stack *stack, t_node *node) {
-  t_node *head = stack->head;
-  if (head == NULL) {
-    head = node;
+  if (stack->head == NULL) {
+    stack->head = node;
+    stack->size++;
     return;
   }
-  t_node *last = head->prev;
-  node->next = head;
+  t_node *last = stack->head->prev;
+  node->next = stack->head;
   node->prev = last;
-  head->prev = node;
+  stack->head->prev = node;
   last->next = node;
-  head = node;
+  stack->head = node;
+  stack->size++;
 }
