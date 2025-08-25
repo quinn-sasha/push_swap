92102d47d5a34be2b08c25b317cda13fa8b9cac9
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-08-25 21:21:24 +0900

    Modified: delete num_arguments variable

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 13:48:59 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/25 20:30:49 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/25 21:13:12 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,9 @@ int main(int argc, char *argv[]) {
     return;
 
   // TODO: is_sorted() check
-  int num_arguments = argc - 1;
   t_stack stack1;
   t_stack stack2;
-  initialize_stack1(&stack1, num_arguments, argv + 1);
+  initialize_stack1(&stack1, argc - 1, argv + 1);
   initialize_stack2(&stack2);
   sort(&stack1, &stack2);
   free(stack1.nodes);
