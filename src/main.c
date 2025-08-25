d50fa7a570ef73c44e0412ecb651cecff4f9eae8
quinn-sasha <quinnsashakenta@gmail.com>
2025-08-25 09:40:49 +0900

    Deleted: max_index

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 13:48:59 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/25 09:26:13 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/25 09:40:22 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
   t_stack stack1;
   t_stack stack2;
   initialize_stack1(&stack1, num_arguments, argv + 1);
-  initialize_stack2(&stack2, num_arguments);
+  initialize_stack2(&stack2);
   sort(&stack1, &stack2);
   // free stack1
 }
