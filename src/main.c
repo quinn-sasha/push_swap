dbde953a284de901442541b616aa2650799f24ff
quinn-sasha <s.kuin.24.5.1435@4nonome.com>
2025-08-26 19:12:32 +0900

    Added: validation of input

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: squinn <squinn@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 13:48:59 by squinn            #+#    #+#             */
-/*   Updated: 2025/08/25 21:13:12 by squinn           ###   ########.fr       */
+/*   Updated: 2025/08/26 19:10:19 by squinn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int main(int argc, char *argv[]) {
   if (argc == 1)
-    return;
-
-  // TODO: is_sorted() check
+    exit(EXIT_SUCCESS);
+  if (argv[0][1] == '\0')
+    exit(EXIT_SUCCESS);
+  validate_input(argc, argv);
   t_stack stack1;
   t_stack stack2;
   initialize_stack1(&stack1, argc - 1, argv + 1);
